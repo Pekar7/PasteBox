625b7fb168a7f3e546fdc75487e33e1d91712121
mac <m1901321@edu.misis.ru>
2022-01-29 20:34:14 +0300

    init

---
+++ src/test/java/com/example/pasteBox/PasteBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pasteBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
